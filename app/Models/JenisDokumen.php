1d5ed8f103e1d5ae06a71c4d169ba75b83813286
edithpcr <edith24si@pcr.ac.id>
2025-11-23 12:46:42 +0700

    pagination,filter tahap 2 (jenisdokumen)

--- app/Models/JenisDokumen.php
+++ app/Models/JenisDokumen.php
@@ -1,7 +1,7 @@
 <?php
-
 namespace App\Models;
 
+use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -12,4 +12,14 @@ class JenisDokumen extends Model
     protected $table = 'jenis_dokumen';
 
     protected $fillable = ['nama_jenis', 'deskripsi'];
+
+    public function scopeFilter(Builder $query, $request, array $filterableColumns): Builder
+    {
+        foreach ($filterableColumns as $column) {
+            if ($request->filled($column)) {
+                $query->where($column, $request->input($column));
+            }
+        }
+        return $query;
+    }
 }
